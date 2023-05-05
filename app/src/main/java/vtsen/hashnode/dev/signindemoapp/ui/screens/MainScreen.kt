f220d7ca10335bc9a0e9f48129f0f0c6d7c524a3
vinchamp77 <vinchamp77@gmail.com>
2023-05-05 14:06:29 +1000

    Add sign-in/out and sign-in status

--- app/src/main/java/vtsen/hashnode/dev/signindemoapp/ui/screens/MainScreen.kt
+++ app/src/main/java/vtsen/hashnode/dev/signindemoapp/ui/screens/MainScreen.kt
@@ -2,8 +2,10 @@ package vtsen.hashnode.dev.signindemoapp.ui.screens
 
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +15,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 import vtsen.hashnode.dev.signindemoapp.ui.theme.NewEmptyComposeAppTheme
 
@@ -21,15 +26,37 @@ import vtsen.hashnode.dev.signindemoapp.ui.theme.NewEmptyComposeAppTheme
 fun MainScreen() {
 
     var showSignIn by remember {mutableStateOf(false)}
-    Row(
+    var signInStatus by remember{ mutableStateOf("Not Signed-in") }
+    var userId by remember{ mutableStateOf("") }
+
+    val context = LocalContext.current
+
+    Column(
         modifier = Modifier.fillMaxSize(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+
+        Spacer(Modifier.padding(2.dp))
+        Text("Sign-in Status: $signInStatus")
+
+        Spacer(Modifier.padding(2.dp))
+        Text("User Id: $userId")
+
+        Spacer(Modifier.padding(2.dp))
         Button(onClick = {
             showSignIn = true
         }) {
             Text("Sign In")
+        }
+
+        Spacer(Modifier.padding(2.dp))
+        Button(onClick = {
+            AuthUI.getInstance().signOut(context)
+            signInStatus = "Not Signed-in"
+            userId = ""
+        }) {
+            Text("Sign Out")
         }
     }
 
@@ -37,14 +64,16 @@ fun MainScreen() {
         SignInScreen { result ->
             val response = result.idpResponse
             if (result.resultCode == ComponentActivity.RESULT_OK) {
-                // Successfully signed in
+                signInStatus = "Signed In"
                 val user = FirebaseAuth.getInstance().currentUser
-                // ...
+                userId = user!!.uid
             } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
+                signInStatus = if (response == null) {
+                    "Not Signed In"
+                } else {
+                    val errorCode = response.getError()?.getErrorCode()
+                    "Failed - Error Code: $errorCode"
+                }
             }
             showSignIn = false
         }
